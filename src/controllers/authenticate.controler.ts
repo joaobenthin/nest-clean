3b6205fe67fb13aca9524c8898dca8d501a38c9a
João Benthin <35600583+joaobenthin@users.noreply.github.com>
2024-01-30 14:43:00 -0300

    Configurando Vitest com SWC

--- src/controllers/authenticate.controler.ts
+++ src/controllers/authenticate.controler.ts
@@ -8,8 +8,8 @@ import {
 import { JwtService } from '@nestjs/jwt'
 import { z } from 'zod'
 
-import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
-import { PrismaService } from 'src/prisma/prisma.service'
+import { ZodValidationPipe } from '@/pipes/zod-validation-pipe'
+import { PrismaService } from '@/prisma/prisma.service'
 import { compare } from 'bcryptjs'
 
 const authenticateBodySchema = z.object({
